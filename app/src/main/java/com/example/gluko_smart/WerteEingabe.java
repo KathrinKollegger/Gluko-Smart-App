f6ba7e1972c0188deeefbf1fc401021ece5e7624
mel2306 <melanie.goedl@live.at>
2022-12-05 21:28:52 +0100

    Werte eingabe Layout optimiert und speicher Button eingefügt

--- app/src/main/java/com/example/gluko_smart/WerteEingabe.java
+++ app/src/main/java/com/example/gluko_smart/WerteEingabe.java
@@ -5,19 +5,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+
+import androidx.appcompat.widget.AppCompatSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WerteEingabe extends Activity {
 
     Button button_homeVerlauf;
+    AppCompatSpinner ed_infoessen;
+    private String selectedEssen;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_werte_eingabe);
 
         button_homeVerlauf = (Button) findViewById(R.id.button_homeVerlauf);
+        ed_infoessen = (AppCompatSpinner) findViewById(R.id.ed_infoessen);
 
         button_homeVerlauf.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +37,37 @@ public class WerteEingabe extends Activity {
                 startActivity(intent7);
             }
         });
-    }}
+
+        setupSpinner();
+    }
+    private void setupSpinner() {
+        List<String> categories = new ArrayList<String>();
+        categories.add("Vor dem Essen");
+        categories.add("Nach dem Essen");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ed_infoessen.setAdapter(dataAdapter);
+
+        ed_infoessen.setSelection(0);
+        selectedEssen="Vor dem Essen";
+        ed_infoessen.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            /**
+             * Methode wenn Geschlecht gewählt wurde. Änderung des Begriffs in der View.
+             */
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                selectedEssen=adapterView.getItemAtPosition(position).toString();
+            }
+            /**
+             * Methode wenn kein Geschlecht gewählt wurde.
+             * Die View bleibt so wie sie ist.
+             */
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+}
