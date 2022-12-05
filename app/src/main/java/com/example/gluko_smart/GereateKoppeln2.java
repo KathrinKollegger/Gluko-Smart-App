ccdb4e3bf4a1d404328f0cf9257901dabd00ad83
mel2306 <melanie.goedl@live.at>
2022-12-05 11:32:42 +0100

    Oberfläche weitergemacht und activitys hinzugefügt;

---
+++ app/src/main/java/com/example/gluko_smart/GereateKoppeln2.java
@@ -0,0 +1,14 @@
+package com.example.gluko_smart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GereateKoppeln2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gereate_koppeln2);
+    }
+}
