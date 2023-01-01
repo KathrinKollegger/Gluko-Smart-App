5b7d116a2a4ecfe0b8e3a413a4ee7a9e27f9377b
mel2306 <melanie.goedl@live.at>
2023-01-01 13:11:19 +0100

    BLE weiter probiert; teilweise fehlermeldung bei Bluetoothaktivierung; bei neustart ging es dann aber wieder; Gerät auslesen versucht in extra Datei zu implementieren damit Programm nicht zerstört wird; kleinen Schritt voran gekommen

--- app/src/main/java/com/example/gluko_smart/GereateKoppeln2.java
+++ app/src/main/java/com/example/gluko_smart/GereateKoppeln2.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class GereateKoppeln2 extends AppCompatActivity {
 
     Button button_wertebekommen;
+    Button button_homeKoppeln;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class GereateKoppeln2 extends AppCompatActivity {
         setContentView(R.layout.activity_gereate_koppeln2);
 
         button_wertebekommen = (Button) findViewById(R.id.button_wertebekommen);
+        button_homeKoppeln = (Button) findViewById(R.id.button_homeKoppeln);
 
 
         button_wertebekommen.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +26,15 @@ public class GereateKoppeln2 extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent13 = new Intent(GereateKoppeln2.this, GereateKoppeln.class);
                 startActivity(intent13);
+            }
+        });
+
+
+        button_homeKoppeln.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent14 = new Intent(GereateKoppeln2.this, Home.class);
+                startActivity(intent14);
             }
         });
     }
