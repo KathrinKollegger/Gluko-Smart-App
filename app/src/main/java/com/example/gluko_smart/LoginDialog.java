8fff5cc8b1550e3f6db3d9e21217fddb55a73dd5
waltljoa99 <joachim.waltl99@gmail.com>
2023-01-08 21:51:40 +0100

    App Icon geändert auf Bluttropfen

--- app/src/main/java/com/example/gluko_smart/LoginDialog.java
+++ app/src/main/java/com/example/gluko_smart/LoginDialog.java
@@ -5,10 +5,12 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class LoginDialog extends DialogFragment {
+    @NonNull
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder dialBuilder = new AlertDialog.Builder(getActivity());
         dialBuilder.setTitle("Login ongoing")
