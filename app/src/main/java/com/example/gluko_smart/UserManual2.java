a440c44111e4599e8296788f314ca3f541b01385
Regina Gugg <regina.gugg@student.tugraz.at>
2023-02-22 15:57:48 +0100

    Integration User-Handbuch; Zoom-Funktion fehlt noch

--- app/src/main/java/com/example/gluko_smart/UserManual2.java
+++ app/src/main/java/com/example/gluko_smart/UserManual2.java
@@ -2,23 +2,50 @@ package com.example.gluko_smart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
 public class UserManual2 extends AppCompatActivity {
 
+    private static final String FILE_NAME = "usermanual.pdf";
+    private static final int PAGE_INDEX = 0;
     Button button_homeUser;
+    ImageView imageView;
+    ImageButton button_next, button_previous;
 
+    private int currentPage = 0;
+    private int pageCount = 0;
+    private PdfRenderer renderer;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usermanual2);
 
+        imageView = findViewById(R.id.pdf_image);
+        button_next = findViewById(R.id.pdfbutton_next);
+        button_previous = findViewById(R.id.pdfbutton_back);
 
-        button_homeUser = (Button) findViewById(R.id.button_homeUser);
-
+        button_homeUser = findViewById(R.id.button_homeUser);
         button_homeUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +53,99 @@ public class UserManual2 extends AppCompatActivity {
                 startActivity(intent9);
             }
         });
+
+        pageCount = getPageCount(this);
+        if (pageCount > 0) {
+            displayPdf(this, imageView, currentPage);
+
+            // OnClickListener hinzufügen, um durch PDF zu navigieren
+            button_previous.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (currentPage > 0) {
+                        currentPage--;
+                        displayPdf(UserManual2.this, imageView, currentPage);
+                    }
+                }
+            });
+
+            button_next.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (currentPage < pageCount - 1) {
+                        currentPage++;
+                        displayPdf(UserManual2.this, imageView, currentPage);
+                    }
+                }
+            });
+        }
+    }
+
+    public static int getPageCount(Context context) {
+        try{
+            File file = new File(context.getFilesDir(), FILE_NAME);
+
+            if(!file.exists()){
+                copyFileFromAssets(context, FILE_NAME, file);
+            }
+
+            // PDF-Datei öffnen und Seitenzahl abrufen
+            ParcelFileDescriptor fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+            PdfRenderer renderer = new PdfRenderer(fileDescriptor);
+            final int pageCount = renderer.getPageCount();
+
+            // Aufräumen
+            renderer.close();
+            fileDescriptor.close();
+
+            return pageCount;
+        } catch (IOException ex){
+            ex.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static void displayPdf (Context context, ImageView imageView, int pageIndex){
+        try{
+            File file = new File(context.getFilesDir(), FILE_NAME);
+
+            if(!file.exists()){
+                copyFileFromAssets(context, FILE_NAME, file);
+            }
+
+            // PDF-Datei öffnen und Seitenzahl abrufen
+            ParcelFileDescriptor fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+            PdfRenderer renderer = new PdfRenderer(fileDescriptor);
+
+            // Bitmap erstellen und PDF-Seite rendern
+            PdfRenderer.Page page = renderer.openPage(pageIndex);
+            Bitmap bitmap = Bitmap.createBitmap(page.getWidth(), page.getHeight(), Bitmap.Config.ARGB_8888);
+            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+            imageView.setImageBitmap(bitmap); // Bitmap im ImageView anzeigen
+            page.close();
+
+            // Aufräumen
+            renderer.close();
+            fileDescriptor.close();
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private static void copyFileFromAssets(Context context, String fileName, File file) throws IOException {
+        AssetManager assetManager = context.getAssets();
+        InputStream inputStream = assetManager.open(fileName);
+        FileOutputStream outputStream = new FileOutputStream(file);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
     }
 }
